eee0a9170c7fccfbc09725635fc4e2fc3d319f67
Van Cauwenberghe Tom <tom.vancauwenberghe@cegeka.com>
2018-05-14 20:33:22 +0200

    first commit

---
+++ FailingWebApi/Controllers/TimeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Http;
+
+namespace FailingWebApi.Controllers
+{
+    public class TimeController : ApiController
+    {
+        public CurrentTime Get()
+        {
+            var currentTime = new CurrentTime
+            {
+                Now = DateTime.Now.ToLongTimeString(),
+                Provider = "TimeController"
+            };
+            return currentTime;
+        }
+    }
+
+    public class CurrentTime
+    {
+        public string Now { get; set; }
+        public string Provider { get; set; }
+    }
+}
