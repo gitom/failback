646864b14f59c29b5fbd900a82e3657e1441c020
Van Cauwenberghe Tom <tom.vancauwenberghe@cegeka.com>
2018-05-14 21:33:42 +0200

    add launchdarkly

--- FailingWebApi/Controllers/TimeController.cs
+++ FailingWebApi/Controllers/TimeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web.Http;
+using LaunchDarkly.Client;
 
 namespace FailingWebApi.Controllers
 {
@@ -7,10 +8,18 @@ namespace FailingWebApi.Controllers
     {
         public CurrentTime Get()
         {
+            //LdClient client = new LdClient("sdk-6337c39c-9bd1-4365-9b70-38ce60632d99");
+            //User user = LaunchDarkly.Client.User.WithKey("bob@example.com")
+            //    .AndFirstName("Bob")
+            //    .AndLastName("Loblaw")
+            //    .AndCustomAttribute("groups", "beta_testers");
+
+            //var toggle = client.BoolVariation("test", user);
             var currentTime = new CurrentTime
             {
                 Now = DateTime.Now.ToLongTimeString(),
-                Provider = "Public api (but sometimes I fail)"
+                Provider = "Public api (but sometimes I fail)",
+                //Toggle = toggle
             };
             return currentTime;
         }
@@ -20,5 +29,6 @@ namespace FailingWebApi.Controllers
     {
         public string Now { get; set; }
         public string Provider { get; set; }
+        public bool Toggle { get; set; }
     }
 }
