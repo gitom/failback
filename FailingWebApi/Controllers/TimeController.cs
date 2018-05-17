831e2bc28ff73495b8f0296273243dd49a186244
Glenn Van Weyenberg <glennvanweyenberg@gmail.com>
2018-05-17 18:52:58 +0200

    Added the launchdarkley code to check a basic feature toggle

--- FailingWebApi/Controllers/TimeController.cs
+++ FailingWebApi/Controllers/TimeController.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Web.Http;
+using LaunchDarkly.Client;
 
 namespace FailingWebApi.Controllers
 {
     public class TimeController : ApiController
     {
+        private readonly LdClient ldClient = new LdClient("sdk-6337c39c-9bd1-4365-9b70-38ce60632d99");
+
         public CurrentTime Get()
         {
-            var currentTime = new CurrentTime
+            var user = LaunchDarkly.Client.User.WithKey("bob@example.com");
+
+            bool showFeature = ldClient.BoolVariation("break-dependency", user, false);
+
+            if (showFeature)
             {
-                Now = DateTime.Now.ToLongTimeString(),
-                Provider = "Public api (but sometimes I fail)"
-            };
-            return currentTime;
+                throw new Exception("This api is just bad...");
+            }
+            else
+            {
+                return new CurrentTime
+                {
+                    Now = DateTime.Now.ToLongTimeString(),
+                    Provider = "Public api (but sometimes I fail)"
+                };
+            }
         }
     }
 
